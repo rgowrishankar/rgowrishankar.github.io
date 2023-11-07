9a1290312fefef2aeb681acce3870757d41505da
Ramkumar Gowrishankar <ramkumar.gowrishankar@ibm.com>
2023-11-07 14:30:39 -0500

    checkpoint2-plot 2 numswq

--- assets/js/compound_interest.js
+++ assets/js/compound_interest.js
@@ -1,16 +1,5 @@
 var yearlyContribution=Math.floor(Math.random() * 10) + 1;
 var xyValues = [
-  {x:50, y:yearlyContribution},
-  {x:60, y:8},
-  {x:70, y:8},
-  {x:80, y:9},
-  {x:90, y:9},
-  {x:100, y:9},
-  {x:110, y:10},
-  {x:120, y:11},
-  {x:130, y:14},
-  {x:140, y:14},
-  {x:150, y:15}
 ];
 var ctx = document.getElementById("myCIChart").getContext('2d');
 var ciChart = new Chart(ctx, {
@@ -30,3 +19,12 @@ var ciChart = new Chart(ctx, {
   }
 });
 ciChart.update()
+function plotNums(){
+  var n1= parseInt(document.getElementById("firstNum").value);
+  var n2= parseInt(document.getElementById("secondNum").value);
+  var xyValues = [
+  {x:50, y:n1},
+  {x:60, y:n2}];
+  ciChart.data.datasets[0].data=xyValues;
+  ciChart.update();
+}
