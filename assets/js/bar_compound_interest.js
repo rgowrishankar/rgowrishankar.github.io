d9f7464254b0ebe7bc87eb216d13cd0b76971810
Ramkumar Gowrishankar <ramkumar.gowrishankar@ibm.com>
2023-11-08 17:41:09 -0500

    Rename the calculator

--- assets/js/bar_compound_interest.js
+++ assets/js/bar_compound_interest.js
@@ -2,11 +2,21 @@ var labels = [];
 var data = {
   labels: labels,
   datasets: [{
-    label: 'Compound Interest Bar Chart',
+    label: 'Balance with withdrawals',
     data: [],
     borderWidth: 1,
-    borderColor: 'blue',
-    backgroundColor: 'blue',
+    borderColor: '#82A3A1',
+    backgroundColor: '#82A3A1',
+    ticks: {
+      beginAtZero: true
+    }
+  },
+  {
+    label: 'Balance without withdrawals',
+    data: [],
+    borderWidth: 1,
+    borderColor: '#2EC4B6',
+    backgroundColor: '#2EC4B6',
     ticks: {
       beginAtZero: true
     }
@@ -24,9 +34,9 @@ const config = {
         display: true,
         color: 'red',
         grid: {
-          color: 'red',
-          borderColor: 'red',
-          tickColor: 'red'
+          color: '#9FC490',
+          borderColor: '#9FC490',
+          tickColor: '#9FC490'
         },
         ticks: {
           beginAtZero: true
@@ -44,18 +54,32 @@ function plotNums(){
   var interestRate= parseInt(document.getElementById("interestRate").value);
   var initialBalance = parseInt(document.getElementById("initialBalance").value);
   var annualContribution = parseInt(document.getElementById("annualContribution").value);
+  var numYearsToContrib= parseInt(document.getElementById("numYearsContrib").value);
+  var numYearsToStartWithdraw = parseInt(document.getElementById("numYearsToStartWithdraw").value);
+  var withdrawalAmount = parseInt(document.getElementById("withdrawalAmount").value);
+  var inflationRate = parseInt(document.getElementById("inflationRate").value);
   var labelArray=Array.from({ length: numYears+1 }, (value, index) => index);
   ciChart.data.labels=labelArray;
   //ciChart.data.datasets[0].data=labelArray;
-  ciChart.data.datasets[0].data= calculate_return(initialBalance, annualContribution, interestRate, numYears);
+  ciChart.data.datasets[0].data= calculate_return(initialBalance, annualContribution, interestRate, numYears, numYearsToContrib, numYearsToStartWithdraw, withdrawalAmount, inflationRate);
+  ciChart.data.datasets[1].data= calculate_return(initialBalance, annualContribution, interestRate, numYears, numYearsToContrib,numYears, 0, 0);
   ciChart.update();
 }
 
-function calculate_return(initialBalance, annualContribution, interestRate, numYears) {
+function calculate_return(initialBalance, annualContribution, interestRate, numYears, numYearsToContrib, numYearsToStartWithdraw, withdrawalAmount, inflationRate) {
   let result=[initialBalance];
   let currentBalance=initialBalance;
+  let inflatedWithdrawalAmount = withdrawalAmount
   for (let i = 0; i < numYears; i++) {
-    currentBalance = (currentBalance*(1+(interestRate/100)))+annualContribution;
+    inflatedWithdrawalAmount = inflatedWithdrawalAmount *(1 + inflationRate/100);
+    if (i <= numYearsToContrib) {
+      currentBalance = (currentBalance*(1+(interestRate/100)))+annualContribution;
+    } else {
+      currentBalance = (currentBalance*(1+(interestRate/100)));
+    }
+    if (i >= numYearsToStartWithdraw) {
+      currentBalance = currentBalance - inflatedWithdrawalAmount;
+    }
     result.push(currentBalance);
   }
   return result;
